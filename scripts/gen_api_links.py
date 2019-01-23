4dc245bbb9797e6d1111e6d965b35ead842c2488
bashbaug <ben.ashbaugh@intel.com>
2019-01-22 17:42:35 -0800

    added scripts to generate link documents

---
+++ scripts/gen_api_links.py
@@ -0,0 +1,137 @@
+# Copyright (c) 2019 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import os
+import re
+
+# File Header:
+def GetHeader():
+    return """// Copyright 2017-2019 The Khronos Group. This work is licensed under a
+// Creative Commons Attribution 4.0 International License; see
+// http://creativecommons.org/licenses/by/4.0/
+
+"""
+
+# File Footer:
+def GetFooter():
+    return """
+"""
+
+printHelp = False
+
+nameFuncsDoc = 'api-funcs.asciidoc'
+nameEnumsDoc = 'api-enums.asciidoc'
+
+nameFuncsDict = 'api-funcs.list'
+nameEnumsDict = 'api-enums.list'
+
+if ( len(sys.argv) == 2 ) and ( sys.argv[1] == '-h' or sys.argv[1] == '-?' ):
+    printHelp = True
+
+if printHelp:
+    print('usage: gen_api_links.py')
+elif not os.path.exists(nameFuncsDict) or not os.path.exists(nameEnumsDict):
+    print('error: dictionary file ' + nameFuncsDict + ' or ' + nameEnumsDict + ' does not exist!')
+else:
+    print('Generating funcs from source: ' + nameFuncsDict)
+
+    srcFile = open(nameFuncsDict, 'rU')
+    docFile = open(nameFuncsDoc, 'w')
+
+    docFile.write( GetHeader() )
+
+    numberOfFuncs = 0
+
+    for line in srcFile:
+        # Skip empty lines or whitespace lines:
+        if not line or line.isspace():
+            continue
+
+        # Skip comment lines (lines beginning with '#'):
+        comment = re.search("^#.*", line)
+        if comment:
+            continue
+
+        # Strip the trailing newline:
+        line=line[0:-1]
+
+        # Example:
+        #
+        # // clEnqueueNDRangeKernel
+        # :clEnqueueNDRangeKernel_label: pass:q[*clEnqueueNDRangeKernel*]
+        # :clEnqueueNDRangeKernel: <<clEnqueueNDRangeKernel,{clEnqueueNDRangeKernel_label}>>
+        docFile.write('// ' + line + '\n')
+        docFile.write(':' + line + '_label: pass:q[*' + line + '*]\n')
+        docFile.write(':' + line + ': <<' + line + ',{' + line + '_label}>>\n')
+        docFile.write('\n')
+
+        numberOfFuncs = numberOfFuncs + 1
+
+        #print('Not sure what to do with: ' + line)
+
+    docFile.write( GetFooter() )
+
+    srcFile.close()
+    docFile.close()
+
+    print('Successfully generated file: ' + nameFuncsDoc)
+    print('Found ' + str(numberOfFuncs) + ' API functions.')
+
+
+    print('Generating enums from source: ' + nameEnumsDict)
+
+    srcFile = open(nameEnumsDict, 'rU')
+    docFile = open(nameEnumsDoc, 'w')
+
+    docFile.write( GetHeader() )
+
+    numberOfEnums = 0
+
+    for line in srcFile:
+        # Skip empty lines or whitespace lines:
+        if not line or line.isspace():
+            continue
+
+        # Skip comment lines (lines beginning with '#'):
+        comment = re.search("^#.*", line)
+        if comment:
+            continue
+
+        # Strip the trailing newline:
+        line=line[0:-1]
+
+        # Example:
+        #
+        # // CL_MEM_READ_ONLY
+        #:CL_MEM_READ_ONLY_label: pass:q[`CL_MEM_READ_ONLY`]
+        #:CL_MEM_READ_ONLY: <<CL_MEM_READ_ONLY,{CL_MEM_READ_ONLY_label}>>
+        #:CL_MEM_READ_ONLY_anchor: [[CL_MEM_READ_ONLY]]{CL_MEM_READ_ONLY}
+        docFile.write('// ' + line + '\n')
+        docFile.write(':' + line + '_label: pass:q[`' + line + '`]\n')
+        docFile.write(':' + line + ': <<' + line + ',{' + line + '_label}>>\n')
+        docFile.write(':' + line + '_anchor: [[' + line + ']]{' + line + '}\n')
+        docFile.write('\n')
+
+        numberOfEnums = numberOfEnums + 1
+
+        #print('Not sure what to do with: ' + line)
+
+    docFile.write( GetFooter() )
+
+    srcFile.close()
+    docFile.close()
+
+    print('Successfully generated file: ' + nameEnumsDoc)
+    print('Found ' + str(numberOfEnums) + ' API enumerations.')
