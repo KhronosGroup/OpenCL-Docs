4b16eb4af7eee2946e0e16ca7df978e1f4344bd7
Alastair Murray <alastairmurray42@gmail.com>
2019-10-28 18:09:43 +0000

    Update scripts/gen_version_notes.py
    Co-Authored-By: Alexey Sachkov <alexey.sachkov@intel.com>

--- scripts/gen_version_notes.py
+++ scripts/gen_version_notes.py
@@ -127,7 +127,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     specpath = args.registry
-    #specpath = "https://raw.githubusercontent.com/KhronosGroup/OpenCL-Registry/master/xml/cl.xml"
 
     print('Generating version notes from: ' + specpath)
 
