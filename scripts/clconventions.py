a5bba585753f270380f3805687e6acb87e7b5131
Jon Leech <oddhack@sonic.net>
2021-09-12 03:01:11 -0700

    First cut at 3.0 refpages
    Import relevant changes from update-refpages-2.2-from-0a92eee branch
    
    Update copyrights on relevant files to use SPDX license identifiers
    (should do this for all files and make repository REUSE-compliant, but
    that's a different issue).
    
    Update refpage generation scripts to use 3.0 specs / version numbers
    instead of 2.2.

--- scripts/clconventions.py
+++ scripts/clconventions.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3 -i
 #
-# Copyright (c) 2013-2021 The Khronos Group Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2013-2021 The Khronos Group Inc.
+# SPDX-License-Identifier: Apache-2.0
 
 # Working-group-specific style conventions,
 # used in generation.
@@ -186,9 +175,9 @@ class OpenCLConventions(ConventionsBase):
            None.
         """
         if spectype == 'api' or spectype is None:
-            return 'https://www.khronos.org/registry/OpenCL/specs/2.2/html/OpenCL_API.html'
+            return 'https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_API.html'
         elif spectype == 'clang':
-            return 'https://www.khronos.org/registry/OpenCL/specs/2.2/html/OpenCL_C.html'
+            return 'https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_C.html'
         else:
             return None
 
