eaac933fd65cfc2d47de43fecb99b03df9a88120
Alastair Murray <alastair.murray@codeplay.com>
2022-04-29 19:11:14 +0100

    Update Khronos Group copyrights to 2022 (#757)
    This PR does not change non-Khronos Group copyrights.
    
    For reference in 2023: I updated the copyrights using the below command and
    then manually checked the result -- `cl_extension_template.asciidoc` is the
    only file with an oddity in that regard.
    
    ```sh
    find . \
         -type f \
         ! -path './.git/*' \
         ! -path './generated/*' \
         ! -path './out/*' \
         -exec sed -i {} -e 's/\(Copyright.*20[0-9][0-9]\)-2021/\1-2022/g' \
                         -e 's/\(Copyright.*2021\)/\1-2022/g' \;
    ```

--- scripts/gen_version_notes.py
+++ scripts/gen_version_notes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2019-2021 The Khronos Group Inc.
+# Copyright 2019-2022 The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
 from collections import OrderedDict
@@ -23,7 +23,7 @@ def parse_xml(path):
 
 # File Header:
 def GetHeader():
-    return """// Copyright 2017-2021 The Khronos Group. This work is licensed under a
+    return """// Copyright 2017-2022 The Khronos Group. This work is licensed under a
 // Creative Commons Attribution 4.0 International License; see
 // http://creativecommons.org/licenses/by/4.0/
 """
